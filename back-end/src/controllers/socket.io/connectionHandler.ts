e87cee25d8915aa3a8b8dbe0472638ecd21befbd
Muhammed Shuhaib <mshuhaibkvl@gmail.com>
2024-12-20 09:57:40 +0530

    origins-updated-agin

--- back-end/src/controllers/socket.io/connectionHandler.ts
+++ back-end/src/controllers/socket.io/connectionHandler.ts
@@ -13,7 +13,7 @@ const server = http.createServer(app)
 const io = new Server(server,{
     cors: {
       origin: process.env.CORS_ORIGIN,
-      methods: ["GET", "POST"]
+      methods: ["GET", "POST",'PUT', 'DELETE']
     },
     maxHttpBufferSize: 25 * 1024 * 1024,
 });
